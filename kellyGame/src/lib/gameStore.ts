5dd57bfd174410127199512220adfeb5c5a369a0
dlwjiang <dlwjiang@gmail.com>
2022-07-27 14:43:30 -0700

    basic store working

---
+++ kellyGame/src/lib/gameStore.ts
@@ -0,0 +1,62 @@
+import { derived, writable } from 'svelte/store';
+
+const INITIAL_BALANCE = 100;
+const WIN_PERCENT = 0.6;
+
+function createGameState() {
+	// const { subscribe: balance, set: setBalance, update: updateBalance } = writable(INITIAL_BALANCE);
+
+	// const { subscribe: history, set: setHistory, update: updateHistory } = writable<number[]>([]);
+
+	const balanceStore = writable(INITIAL_BALANCE);
+	const { subscribe: balance, set: setBalance, update: updateBalance } = balanceStore;
+
+	const historyStore = writable<number[]>([]);
+	const { subscribe: history, set: setHistory, update: updateHistory } = historyStore;
+
+	/**
+	 * Bet a percentage (0-1) of the current balance. Double the winnings if the bet is won.
+	 * Else lose everything that was bet.
+	 */
+	const betPercentage = (percent: number) => {
+		const rand = Math.random();
+		const win = rand < WIN_PERCENT;
+		if (win) {
+			updateBalance((balance) => {
+				return balance * (1 - percent) + percent * balance * 2;
+			});
+			updateHistory((history) => {
+				return [...history, 1]; // 1 means win
+			});
+		} else {
+			updateBalance((balance) => {
+				return balance * (1 - percent);
+			});
+			updateHistory((history) => {
+				return [...history, 0]; // 0 means loss
+			});
+		}
+	};
+
+	const reset = () => {
+		setBalance(INITIAL_BALANCE);
+		setHistory([]);
+	};
+
+	const state = derived([balanceStore, historyStore], ([$balance, $history]) => {
+		return {
+			balance: $balance,
+			history: $history
+		};
+	});
+
+	return {
+		subscribe: state.subscribe,
+		betPercentage,
+		reset
+	};
+}
+
+export const count = createGameState();
+
+export default count;
---
+++ kellyGame/src/routes/kellyGame/index.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import gameStore from '$lib/gameStore';
+
+	const bet = () => {
+		gameStore.betPercentage(0.2);
+	};
+</script>
+
+<div class="container">
+	<h1>Kelly Criterion Game</h1>
+	<div class="content">
+		<div class="controls">
+			<h2>
+				Current Balance: {$gameStore.balance}
+			</h2>
+			<p>
+				Each round you have a 60% chance to win and double what you bet and a corresponding 40%
+				chance of losing all of what you bet. How would you bet in order to maximize the amount of
+				money in the shortest amount of time?
+			</p>
+			<div>
+				{#each $gameStore.history as val}
+					<div>{val}</div>
+				{/each}
+			</div>
+			<div>
+				<div>You are betting ....</div>
+				<div on:click={bet}>bet</div>
+				<div>CARDs</div>
+			</div>
+		</div>
+		<div>Chart</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		max-width: 2560px;
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+
+	.content {
+		display: flex;
+	}
+
+	.content > div {
+		flex-basis: 50%;
+	}
+</style>
